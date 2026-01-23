82c57c0f60a70fefcb80cef88dc8c3081ee8af5a
NicolasPalsa <s2401220@edu.bc.fi>
2026-01-23 12:38:41 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Tervehdys from './Tervehdys.jsx'
+ 
+function App() {
+  return(
+    <div>
+      <Tervehdys name='Nicolas'/>
+    </div>
+  )
+}
+ 
+export default App
---
+++ src/Tervehdys.jsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+ 
+function Tervehdys({name}) {
+    return(
+      <p>Hei {name}!</p>
+    )
+}
+ 
+export default Tervehdys
